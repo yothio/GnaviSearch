0b4bee23e28d0098065cf8379410d8d1fef5a7e4
shiota yoshiki <shiotyan317@gmail.com>
2017-11-24 08:16:14 +0900

    no message

---
+++ app/src/main/java/yothio/gnavisearch/network/API_KEY.java
@@ -0,0 +1,10 @@
+package yothio.gnavisearch.network;
+
+/**
+ * Created by yocchi on 2017/11/13.
+ */
+
+public class API_KEY {
+    public static final String API_KEY = "b6a7071f10abcc2f6c94c14df171ed25";
+    public static final String MAP_KEY = "AIzaSyB7rSxKr9jqxeXMWIwjGGwPl_xElbn0b_c";
+}
