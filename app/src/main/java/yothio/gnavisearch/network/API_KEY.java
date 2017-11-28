85a663e6e7c8791c9222ed5a613a9d0d52cd75dc
shiota yoshiki <shiotyan317@gmail.com>
2017-11-28 19:27:44 +0900

    API_KEYの削除・無効化

--- app/src/main/java/yothio/gnavisearch/network/API_KEY.java
+++ app/src/main/java/yothio/gnavisearch/network/API_KEY.java
@@ -5,6 +5,6 @@ package yothio.gnavisearch.network;
  */
 
 public class API_KEY {
-    public static final String API_KEY = "b6a7071f10abcc2f6c94c14df171ed25";
-    public static final String MAP_KEY = "AIzaSyB7rSxKr9jqxeXMWIwjGGwPl_xElbn0b_c";
+    public static final String API_KEY = "";
+    public static final String MAP_KEY = "";
 }
